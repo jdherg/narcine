7357b8ed619c4c770b950437903a7a38f4b49a32
John Hergenroeder <jdherg@gmail.com>
2015-06-03 12:29:54 -0400

    Move test scene out of raycaster module.

--- ray.py
+++ ray.py
@@ -221,33 +221,13 @@ def ascii_display(grid):
     return rep
 
 
-def gen_png(grid):
+def render_png(scene):
+    grid = render(scene)
     return png_writer.gen_png_data(grid)
 
 
-def make_scene():
-    scene = Scene()
-    s1 = Sphere((0, 0, 0), 5, (255, 0, 0))
-    s2 = Sphere((-2, -2, 10), 5, (0, 0, 255))
-    scene.add_object(s1)
-    scene.add_object(s2)
-    p1 = Plane((4, 4, 15), (5, 5, -5), (0, 255, 0))
-    scene.add_object(p1)
-    camera = Camera((0, 0, -40), (0, 0, 1))
-    scene.add_camera(camera)
-    light1 = Light((8, -8, -8))
-    scene.add_light(light1)
-    light2 = Light((-8, 8, -8))
-    scene.add_light(light2)
-    return scene
-
-
 def main():
-    scene = make_scene()
-    grid = render(scene)
-    png_data = gen_png(grid)
-    with open('tmp.png', 'wb') as f:
-        f.write(png_data)
+    pass
 
 if __name__ == '__main__':
     main()
