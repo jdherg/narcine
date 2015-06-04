f1175a6489803523e7ed0b69996eeb986cc8aa2d
John Hergenroeder <jdherg@gmail.com>
2015-06-03 21:52:28 -0400

    Add additional elements to test scene.

--- test_scene.py
+++ test_scene.py
@@ -3,18 +3,22 @@ from ray import Camera, Light, Plane, Scene, Sphere, render_png
 
 def make_scene():
     scene = Scene()
-    s1 = Sphere((0, 0, 0), 5, (255, 0, 0))
-    s2 = Sphere((-2, -2, 10), 5, (0, 0, 255))
+    s1 = Sphere((-5, -5, 0), 5, (255, 0, 0))
+    s2 = Sphere((5, -5, 10), 5, (0, 255, 0))
+    s3 = Sphere((5, 5, 0), 5, (0, 0, 255))
+    s4 = Sphere((-5, 5, 10), 5, (0, 0, 0))
+    s5 = Sphere((0, 0, 20), 5, (255, 255, 255))
     scene.add_object(s1)
     scene.add_object(s2)
-    p1 = Plane((4, 4, 15), (5, 5, -5), (0, 255, 0))
+    scene.add_object(s3)
+    scene.add_object(s4)
+    scene.add_object(s5)
+    p1 = Plane((0, 0, 40), (0, 0, -1), (128, 128, 128))
     scene.add_object(p1)
     camera = Camera((0, 0, -40), (0, 0, 1))
     scene.add_camera(camera)
-    light1 = Light((8, -8, -8))
+    light1 = Light((0, 0, -10))
     scene.add_light(light1)
-    light2 = Light((-8, 8, -8))
-    scene.add_light(light2)
     return scene
 
 
