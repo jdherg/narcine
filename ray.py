c8458f4e4a7b5ffcbb8e7bc37a8685bb0b26cdd3
John Hergenroeder <jdherg@gmail.com>
2015-06-08 11:21:16 -0400

    Add some docstrings.

--- ray.py
+++ ray.py
@@ -5,8 +5,13 @@ from random import randint
 
 
 class Point():
+    """A class with methods for treating 3-tuples of numbers
+    as points (x, y, z).
+    """
+
     @classmethod
     def distance(cls, start, end):
+        """Finds the distance between two points."""
         return sqrt((start[0] - end[0])**2 +
                     (start[1] - end[1])**2 +
                     (start[2] - end[2])**2)
@@ -19,18 +24,27 @@ class Point():
 
 
 class Vector():
+    """A class with methods for treating 3-tuples of numbers
+       as vectors (x, y, z).
+       """
+
     @classmethod
     def make_vec(cls, terminal, origin):
+        """Given an end and start point, returns a tuple representing
+        that vector translated to the origin.
+        """
         return (terminal[0] - origin[0],
                 terminal[1] - origin[1],
                 terminal[2] - origin[2])
 
     @classmethod
     def length(cls, vec):
+        """Finds the length of a given vector."""
         return sqrt(vec[0]**2 + vec[1]**2 + vec[2]**2)
 
     @classmethod
     def unit(cls, vec):
+        """Returns a unit vector in the same direction as a given vector."""
         length = Vector.length(vec)
         return (
             vec[0] / length,
@@ -39,12 +53,14 @@ class Vector():
 
     @classmethod
     def dot(cls, first, second):
+        """Finds the dot product of two vectors."""
         return first[0] * second[0] + \
-               first[1] * second[1] + \
-               first[2] * second[2]
+            first[1] * second[1] + \
+            first[2] * second[2]
 
     @classmethod
     def angle_ish(cls, first, second):
+        """Used in calculating diffuse light level. Deprecated."""
         distance = Point.distance(Vector.unit(first), Vector.unit(second))
         return distance * pi / 2.0
 
@@ -56,15 +72,22 @@ class Vector():
 
 
 class Ray():
+    """A ray starting at the given location
+    and extending in the given direction.
+    """
+
     def __init__(self, origin, direction):
         self.origin = origin
         self.direction = Vector.unit(direction)
 
     def intersect_to_point(self, t):
+        """Returns a point given a distance along the ray."""
         return Point.add(self.origin, Vector.scale(self.direction, t))
 
 
 class Sphere():
+    """Represents a sphere in a 3d coordinate space."""
+
     def __init__(self, center, radius, color=None):
         self.center = center
         self.radius = radius
@@ -73,6 +96,10 @@ class Sphere():
         self.color = color
 
     def ray_intersect(self, ray):
+        """Finds the nearest intersection of a ray with the sphere.
+        Returns a tuple: (distance along the ray, the sphere)
+        or None if the ray and sphere don't intersect.
+        """
         c = self.center
         r = self.radius
         v = Vector.make_vec(ray.origin, c)
@@ -93,10 +120,13 @@ class Sphere():
         return None
 
     def normal(self, loc):
+        """Returns a vector normal to the sphere at the given location."""
         return Vector.make_vec(loc, self.center)
 
 
 class Plane():
+    """Represents a plane in a 3d coordinate space."""
+
     def __init__(self, point, normal, color=None):
         self.point = point
         self._normal = normal
@@ -105,6 +135,10 @@ class Plane():
         self.color = color
 
     def ray_intersect(self, ray):
+        """Finds the nearest intersection of a ray with the plane.
+        Returns a tuple: (distance along the ray, the plane)
+        or None if the ray and plane are parallel.
+        """
         n = self._normal
         p = self.point
         d = ray.direction
@@ -115,15 +149,18 @@ class Plane():
         return (Vector.dot(n, Vector.make_vec(p, o))/denom, self)
 
     def normal(self, point):
+        """Returns a vector normal to the plane at the given location."""
         return self._normal
 
 
 class Camera():
+    """Represents a pinhole camera and view plane in a 3d coordinate space."""
+
     def __init__(self, position, direction, distance=30):
         self.loc = position
         self.direction = Vector.unit(direction)
         self.distance = distance
-        self.pix_dim = 200
+        self.pix_dim = 500
         self.log_dim = 20
         self.y_min = -1 * self.log_dim//2
         self.y_max = self.log_dim//2
@@ -133,13 +170,17 @@ class Camera():
         self.x_inc = (self.x_max - self.x_min)/self.pix_dim
 
     def screen_loc(self, x, y):
+        """Maps the 2d viewplane into the 3d coordinate space."""
         return (x, y, self.loc[2] + self.direction[2] * self.distance)
 
     def ray_to(self, dest):
+        """Returns a ray from the camera to the given point."""
         return Ray(self.loc, Vector.make_vec(dest, self.loc))
 
 
 class Light():
+    """Represents a light in a 3d coordinate space."""
+
     def __init__(self, position, color=None):
         if not color:
             color = (255, 255, 255)
@@ -148,6 +189,10 @@ class Light():
 
 
 class Scene():
+    """Represents a scene to be rendered,
+    including camera, lighting, and objects.
+    """
+
     def __init__(self):
         self.objects = list()
         self.lights = list()
@@ -162,6 +207,7 @@ class Scene():
         self.objects.append(obj)
 
     def calc_lighting(self, obj, ray, intersect):
+        """Finds the color at a given location."""
         base_color = obj.color
         intersect_point = ray.intersect_to_point(intersect)
         normal = obj.normal(intersect_point)
@@ -178,6 +224,10 @@ class Scene():
                 floor(base_color[2] * lightness))
 
     def ray_intersect(self, ray):
+        """Finds the color at the point at which the given ray
+        intersects with the scene.
+        Returns (0, 0, 0) (Black) for no intersection.
+        """
         intersects = list()
         for obj in self.objects:
             intersect = obj.ray_intersect(ray)
@@ -190,6 +240,9 @@ class Scene():
 
 
 def render(scene):
+    """Renders the given scene, producing a list of rows (lists)
+    of color tuples.
+    """
     res = list()
     camera = scene.camera
     for row in range(camera.pix_dim):
@@ -204,6 +257,7 @@ def render(scene):
 
 
 def ascii_display(grid):
+    """Converts a rendered scene into ASCII. Deprecated."""
     rep = ""
     rep += "*" + "-" * len(grid[0]) + "*\n"
     for row in grid:
@@ -222,6 +276,7 @@ def ascii_display(grid):
 
 
 def render_png(scene):
+    """Renders a scene as PNG data."""
     grid = render(scene)
     return png_writer.gen_png_data(grid)
 
